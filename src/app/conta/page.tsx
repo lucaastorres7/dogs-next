ba34a257b9acada0d7bb6c1faa9d07c27b5e76a6
Lucas Torres <lucastorreslr10@gmail.com>
2025-06-27 21:14:32 -0300

    feat: account header

--- src/app/conta/page.tsx
+++ src/app/conta/page.tsx
@@ -1,7 +1,14 @@
-export default async function AccountPage() {
+"use client";
+
+import { useUser } from "@/context/user-context";
+import React from "react";
+
+export default function AccountPage() {
+  const { user } = useUser();
+
   return (
     <div>
-      <h1>Conta</h1>
+      <h1>Conta {user?.name}</h1>
       <p>Esta é a página de conta.</p>
     </div>
   );
